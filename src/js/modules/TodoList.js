0cde25fc4b7982889503da88cf2d0d389cd77713
Destoky <adestoky@gmail.com>
2020-11-20 19:16:23 +0100

    Intégration du template de la todoList

--- src/js/modules/TodoList.js
+++ src/js/modules/TodoList.js
@@ -1,16 +1,21 @@
-import Todo from './Todo.js';
+import Todo from "./Todo";
+import todoListTemplate from "./templates/todoList.js";
 
 export default class TodoList {
   constructor(data) {
     this.el = document.querySelector(data.el);
     this.todos = [];
     this.loadTodos(data.todos);
-    console.table(this.todos);
+    this.template = todoListTemplate;
+    this.render();
   }
 
   loadTodos(todos) {
     for (const todo of todos) {
       this.todos.push(new Todo(todo));
     }
+  }
+  render() {
+    this.el.innerHTML = this.template;
   }
 }

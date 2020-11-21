e65d1a82019ab15fbc72739a924ca3d362e0f755
Destoky <adestoky@gmail.com>
2020-11-21 23:18:47 +0100

    Intégration du template des Todos

--- src/js/modules/TodoList.js
+++ src/js/modules/TodoList.js
@@ -1,9 +1,10 @@
-import Todo from "./Todo";
+import Todo from "./Todo.js";
 import todoListTemplate from "./templates/todoList.js";
 
 export default class TodoList {
   constructor(data) {
     this.el = document.querySelector(data.el);
+    this.listEl;
     this.todos = [];
     this.loadTodos(data.todos);
     this.template = todoListTemplate;
@@ -12,10 +13,15 @@ export default class TodoList {
 
   loadTodos(todos) {
     for (const todo of todos) {
-      this.todos.push(new Todo(todo));
+      this.todos.push(new Todo({ parent: this, todo }));
     }
   }
   render() {
     this.el.innerHTML = this.template;
+    //L'élément .todo-list existe pour le navigateur
+    this.listEl = this.el.querySelector(".todo-list"); //on l'ajouter car il n'existe pas dans le html, juste dans le render
+    for (let todo of this.todos) {
+      todo.render();
+    }
   }
 }
